07c0e6fbd9b2a1b1994c40da3fefe62707e0cb4f
evg1605 <evg1605@gmail.com>
2021-03-07 17:46:28 +0800

    csv source

---
+++ csv_source/impl.go
@@ -0,0 +1,37 @@
+package csv_source
+
+import (
+	"bytes"
+	"encoding/csv"
+	"io"
+	"net/http"
+	"os"
+)
+
+func CsvFromFile(filePath string) (*csv.Reader, error) {
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer csvFile.Close()
+
+	return csvFromReader(csvFile)
+}
+
+func CsvFromWeb(url string) (*csv.Reader, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return csvFromReader(resp.Body)
+}
+
+func csvFromReader(src io.Reader) (*csv.Reader, error) {
+	var csvRaw bytes.Buffer
+	if _, err := io.Copy(&csvRaw, src); err != nil {
+		return nil, err
+	}
+	return csv.NewReader(bytes.NewReader(csvRaw.Bytes())), nil
+}
