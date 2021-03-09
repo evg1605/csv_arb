2d2a9552a2d14dded8d0119eb6aaee1473c1db9f
evg1605 <evg1605@gmail.com>
2021-03-09 10:15:10 +0800

    getitems test

--- converter/converter_test.go
+++ converter/converter_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestGetFieldsIndexes(t *testing.T) {
-	csvData := fmt.Sprintf(`ru,%s,en,%s,fr`, columnDescription, columnName)
+	csvData := fmt.Sprintf(`ru,%s,en,%s,fr,%s`, columnDescription, columnName, columnParameters)
 	r := csv.NewReader(bytes.NewReader([]byte(csvData)))
 	indexes, err := getFieldsIndexes(r, "en")
 
@@ -19,4 +19,38 @@ func TestGetFieldsIndexes(t *testing.T) {
 	require.Equal(t, 3, indexes.name)
 	require.NotNil(t, indexes.description)
 	require.Equal(t, 1, *indexes.description)
+	require.NotNil(t, indexes.parameters)
+	require.Equal(t, 5, *indexes.parameters)
+}
+
+func TestGetItems(t *testing.T) {
+	csvData := fmt.Sprintf(`item1,descr1,par1;par2,val-ru-1,val-en-1
+item2,descr2,para;parb2,val-ru-2,val-en-2`)
+
+	r := csv.NewReader(bytes.NewReader([]byte(csvData)))
+	descriptionInd, parametersInd := 1, 2
+	indexes := &csvIndexes{
+		name:        0,
+		description: &descriptionInd,
+		parameters:  &parametersInd,
+		cultures: map[string]int{
+			"ru": 3,
+			"en": 4,
+		},
+		countFieldsInRow: 5,
+	}
+
+	items, err := getItems(r, indexes)
+	require.NoError(t, err)
+	require.NotNil(t, items)
+	require.Len(t, items, 2)
+
+	require.Contains(t, items, "item1")
+	require.Contains(t, items, "item2")
+
+	require.Contains(t, items["item1"].cultures, "ru")
+	require.Contains(t, items["item1"].cultures, "en")
+
+	require.Contains(t, items["item1"].parameters, "par1")
+	require.Contains(t, items["item1"].parameters, "par2")
 }
--- converter/impl.go
+++ converter/impl.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
+	"strings"
 
 	"github.com/evg1605/csv_arb/csv_source"
 )
@@ -12,6 +14,7 @@ import (
 const (
 	columnName        = "name"
 	columnDescription = "description"
+	columnParameters  = "parameters"
 )
 
 var (
@@ -21,19 +24,16 @@ var (
 type csvIndexes struct {
 	name             int
 	description      *int
-	cultures         []*cultureInd
+	parameters       *int
+	cultures         map[string]int
 	countFieldsInRow int
-}
-
-type cultureInd struct {
-	name string
-	ind  int
 }
 
 type item struct {
 	name        string
 	description string
 	cultures    map[string]string
+	parameters  map[string]struct{}
 }
 
 func WebCsvToArb(csvUrl,
@@ -80,6 +80,15 @@ func csvToArb(r *csv.Reader, arbFolderPath,
 		return err
 	}
 
+	if err := os.RemoveAll(arbFolderPath); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(arbFolderPath, 0777); err != nil {
+		return err
+	}
+
+	//fieldsIndexes.cultures
 	_ = items
 	return nil
 }
@@ -100,16 +109,37 @@ func getItems(r *csv.Reader, fieldsIndexes *csvIndexes) (map[string]*item, error
 			return nil, fmt.Errorf("invalid row with fields count %v, but expect %v: %w", len(row), fieldsIndexes.countFieldsInRow, errInvalidCsvStructure)
 		}
 
+		name := row[fieldsIndexes.name]
+		if _, ok := items[name]; ok {
+			return nil, fmt.Errorf("found more than one key with same name %s: %w", name, errInvalidCsvStructure)
+		}
+
 		i := &item{
-			name:     row[fieldsIndexes.name],
+			name:     name,
 			cultures: make(map[string]string),
 		}
+
 		if fieldsIndexes.description != nil {
 			i.description = row[*fieldsIndexes.description]
 		}
-		for _, ci := range fieldsIndexes.cultures {
-			i.cultures[ci.name] = row[ci.ind]
+
+		if fieldsIndexes.parameters != nil {
+			i.parameters = make(map[string]struct{})
+			parameters := strings.Split(row[*fieldsIndexes.parameters], ";")
+			for _, p := range parameters {
+				pName := strings.Trim(p, " ")
+				if _, ok := i.parameters[pName]; ok {
+					return nil, fmt.Errorf("key %s has more than one parameter with name %s : %w", name, pName, errInvalidCsvStructure)
+				}
+				i.parameters[pName] = struct{}{}
+			}
 		}
+
+		for cn, ci := range fieldsIndexes.cultures {
+			i.cultures[cn] = row[ci]
+		}
+
+		items[name] = i
 	}
 	return items, nil
 }
@@ -117,61 +147,58 @@ func getItems(r *csv.Reader, fieldsIndexes *csvIndexes) (map[string]*item, error
 func getFieldsIndexes(r *csv.Reader, defaultCulture string) (*csvIndexes, error) {
 	// read first row and get indexes of name and description fields
 
-	var culturesInd []*cultureInd
-	var nameInd, descriptionInd *int
+	var nameInd, descriptionInd, parametersInd *int
 
-	allCultures := make(map[string]struct{})
+	cultures := make(map[string]int)
 
 	row, err := r.Read()
 	if err != nil {
 		return nil, err
 	}
 
+	m := map[string]**int{
+		columnName:        &nameInd,
+		columnDescription: &descriptionInd,
+		columnParameters:  &parametersInd,
+	}
+
 	for i, f := range row {
 		if f == "" {
 			continue
 		}
-		iTmp := i
-		switch f {
-		case columnName:
-			if nameInd != nil {
-				return nil, fmt.Errorf("there should only be one column for the name: %w", errInvalidCsvStructure)
-			}
-			nameInd = &iTmp
 
-		case columnDescription:
-			if descriptionInd != nil {
-				return nil, fmt.Errorf("there should only be one column for the description: %w", errInvalidCsvStructure)
-
+		ind, ok := m[f]
+		if ok {
+			if *ind != nil {
+				return nil, fmt.Errorf("there should only be one column for the %s: %w", f, errInvalidCsvStructure)
 			}
-			descriptionInd = &iTmp
-		default:
-			if _, ok := allCultures[f]; ok {
-				return nil, fmt.Errorf("each culture to be represented by only one column (%s): %w", f, errInvalidCsvStructure)
-			}
-			allCultures[f] = struct{}{}
-			culturesInd = append(culturesInd, &cultureInd{
-				name: f,
-				ind:  i,
-			})
+			iTmp := i
+			*ind = &iTmp
+			continue
 		}
+
+		if _, ok := cultures[f]; ok {
+			return nil, fmt.Errorf("each culture to be represented by only one column (%s): %w", f, errInvalidCsvStructure)
+		}
+		cultures[f] = i
 	}
 
 	if nameInd == nil {
 		return nil, fmt.Errorf("csv must have column for name: %w", errInvalidCsvStructure)
 	}
 
-	if len(allCultures) == 0 {
+	if len(cultures) == 0 {
 		return nil, fmt.Errorf("cultures not found: %w", errInvalidCsvStructure)
 	}
 
-	if _, ok := allCultures[defaultCulture]; !ok {
+	if _, ok := cultures[defaultCulture]; !ok {
 		return nil, fmt.Errorf("csv must have column for default culture (%s): %w", defaultCulture, errInvalidCsvStructure)
 	}
 	return &csvIndexes{
 		name:             *nameInd,
 		description:      descriptionInd,
-		cultures:         culturesInd,
+		parameters:       parametersInd,
+		cultures:         cultures,
 		countFieldsInRow: len(row),
 	}, nil
 }
